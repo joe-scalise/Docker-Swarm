6d1b250a70f489ff14c896ca8a1e6af3e3343bb4
Joe Scalise <jscalise@cmu.edu>
2019-07-31 14:22:30 -0400

    Added update.sh

---
+++ update.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+check_exit_status() {
+
+	if [ $? -eq 0 ]
+	then
+	else
+		echo
+		echo "[ERROR] Process Failed! Exiting."
+        exit 1
+
+	fi
+}
+
+hello() {
+
+	echo
+	echo "Hello, $USER. Let's update this system."
+	echo
+}
+
+update() {
+
+    sudo apt-get update;
+	check_exit_status
+
+    sudo apt-get upgrade -y;
+	check_exit_status
+
+    sudo apt-get dist-upgrade -y;
+	check_exit_status
+
+	sudo apt-get autoremove -y;
+	check_exit_status
+
+	sudo apt-get autoclean -y;
+	check_exit_status
+
+	sudo updatedb;
+	check_exit_status
+}
+
+leave() {
+
+	echo "Update Complete."
+	exit
+}
+
+hello
+update
+leave
